38080ed30637dcf11215dd7d11b79ff9cc0037ae
guillermo2005200 <91117754+guillermo2005200@users.noreply.github.com>
2025-05-27 09:00:23 +0200

    version manifest update

--- mqm_document_format/__manifest__.py
+++ mqm_document_format/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Document Format MQM",
     "summary": """Formatos de documentos de MasQModa""",
-    "version": "15.0.1.0.0",
+    "version": "17.0.1.0.0",
     "description": """Formatos de documentos de MasQModa""",
     "author": "Daniel Dominguez",
     "company": "Xtendoo",
